9672aa940ab96473b5f614e99b107f72502b74ff
MrOsteoporosis <sanderverheijen@outlook.com>
2020-11-10 14:47:38 +0100

    yes

--- libasm.h
+++ libasm.h
@@ -6,7 +6,7 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/18 13:59:18 by averheij      #+#    #+#                 */
-/*   Updated: 2020/06/18 14:40:18 by averheij      ########   odam.nl         */
+/*   Updated: 2020/06/18 23:01:39 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include <stdlib.h>
 
-extern size_t	_ft_strlen(const char *str);
+extern size_t	ft_strlen(const char *str);
 extern char		*ft_strcpy(char *dest, const char *src);
 extern int		ft_strcmp(const char *s1, const char *s2);
 extern ssize_t	ft_write(int fd, const void *buf, size_t count);
